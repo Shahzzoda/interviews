d40cfb44e82181ad5776b77dd4bc7990ed6b4f1b
Shahzoda Davlatova <shahzoda01@gmail.com>
2019-11-05 20:47:24 -0500

    code cleaned

--- datastructs/graphs.py
+++ datastructs/graphs.py
@@ -1,5 +1,4 @@
-edges = [(1, 2), (1, 5), (2, 5), (2, 4), (2, 3), (3, 8)]
-
+# Given a list of pairs that represent edges in an undirected graph, create an adj list out of that. 
 class Node:
   def __init__(self, val):
     self.val = val
@@ -11,37 +10,30 @@ class Node:
   def __eq__(self, p2):
     return p2.val == self.val
 
+def repGraph(edges)
+  alist = []
+  for n1, n2 in edges:
+    temp = Node(n1)
+    if alist.count(temp) == 1:
+      i = alist.index(temp)
+      alist[i].adj.append(n2)
+    else:
+      alist.append(temp)
+      i = alist.index(temp)
+      alist[i].adj.append(n2)
+    temp = Node(n2)
+    if alist.count(temp) == 1:
+      i = alist.index(temp)
+      alist[i].adj.append(n1)
+    else:
+      alist.append(temp)
+      i = alist.index(temp)
+      alist[i].adj.append(n1)
+  return alist
 
-alist = []
-for n1, n2 in edges:
-  temp = Node(n1)
-  if alist.count(temp) == 1:
-    i = alist.index(temp)
-    alist[i].adj.append(n2)
-  else:
-    alist.append(temp)
-    i = alist.index(temp)
-    alist[i].adj.append(n2)
-  temp = Node(n2)
-  if alist.count(temp) == 1:
-    i = alist.index(temp)
-    alist[i].adj.append(n1)
-  else:
-    alist.append(temp)
-    i = alist.index(temp)
-    alist[i].adj.append(n1)
-
+# Test the function
+edges = [(1, 2), (1, 5), (2, 5), (2, 4), (2, 3), (3, 8)]
+alist = repGraph(edges)
 for node in alist:
   print node
   
-
-
-
-
-
-
-
-
-
-
-
