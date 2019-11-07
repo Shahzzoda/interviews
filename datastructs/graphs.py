e35ffabe37ac36b70612a0c2ba62a5cab0c845df
Shahzoda Davlatova <shahzoda01@gmail.com>
2019-11-07 00:51:33 -0500

    updated code to use defaultdict

--- datastructs/graphs.py
+++ datastructs/graphs.py
@@ -1,15 +1,17 @@
 from collections import defaultdict
+from queue import Queue as queue
 # Given a list of pairs that represent edges in an undirected graph, create an adj list out of that. 
 edges = [(1, 2), (1, 5), (2, 5), (2, 4), (2, 3), (3, 8)]
 
 def repGraph(eges):
   alist = defaultdict(list)
   for v1, v2 in edges:
-    alist[n1].append(n2)
-    alist[n2].append(n1)
+    alist[v1].append(v2)
+    alist[v2].append(v1)
   print alist
 
 repGraph(edges)
 
-
+# def bfs(edges):
+#   q
 
