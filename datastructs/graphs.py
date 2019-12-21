332a6bcbf2c961857f4f193d4c61086c98b5027e
Shahzoda Davlatova <shahzoda01@gmail.com>
2019-12-21 18:58:33 -0500

    more notes added.

--- datastructs/graphs.py
+++ datastructs/graphs.py
@@ -1,26 +1,30 @@
+# Graphs are great for representing pairwise relationships between objects.
+# In interview problems, you are often given this in a list of pairs that 
+# represent connections between nodes, also known as edges. There are two 
+# main ways of representing graphs, adjacency list and an adjacency matrix.
 from collections import defaultdict
 from collections import deque
-# Given a list of pairs that represent edges in an undirected graph, create an adj list out of that. 
+
 edges = [(1, 2), (1, 5), (2, 5), (2, 4), (2, 3), (3, 8)]
 
-def repGraph(eges):
+def adjListRep(edges):
   alist = defaultdict(list)
   for v1, v2 in edges:
     alist[v1].append(v2)
     alist[v2].append(v1)
-  print(alist)
-  return alist
+  print("adj list:", alist)
+  return alist 
 
-# to do a bfs, we visit nodes closest to current nodes for every node that has not been visited
-# we'll need a set of visited nodes
-# we'll need a queue of keep track of the order 
-# {
-#  n: [n, n, n],
-#  n: [n, n],
-#  n: [n, n, n, n],
-# }
+# There are some basic traversals we also have to know for virtually every 
+# graph problem we will be given. There are two main traversals for graphs: 
+# BFS and DFS. 
+
+
+# Breadth First Traversals: you vist one node, visit all its 
+# neighbors, and then visit the neighbor's neighbors. We do this until we 
+# have visited all the nodes in the trees. 
 def bfs(edges, s):
-  adjList = repGraph(edges)
+  adjList = adjListRep(edges)
   visit = deque()
   visit.append(s)
   visited = set()
@@ -33,21 +37,25 @@ def bfs(edges, s):
       if neighbor not in visited:
         visit.append(neighbor)
 
-# so you are going deepest possible node 
-# coming one level up and visit all of their nodes 
-# then coming back up 
+# Depth First Search  will start at one node and go as deep as it can. So it'll 
+# visit one node, then one of its neighbor, then another one of its neighbor
+# and do that until it gets to a node that has no more unvisited neigbors. 
+# When it has no more unvisited neighbors, it will backtrack to the previously
+# visited node that has neighbors and go deep down until it hits another with 
+# no more neighbors and redo all of that. 
 def dfs(edges, s):
-  adjList = repGraph(edges)
+  adjList = adjListRep(edges)
+  visit = deque()
+  visit.append(s)
   visited = set()
-  toVisit = deque()
-  toVisit.append(s)
-  while len(toVisit) > 0:
-    currNode = toVisit.pop()
+  while len(visit) > 0:
+    currNode = visit.pop()
     print(currNode)
     visited.add(currNode)
     neighbors = adjList[currNode]
     for neighbor in neighbors:
       if neighbor not in visited:
-        toVisit.append(neighbor)
+        visit.append(neighbor)
 
-dfs(edges, 2)
+
+dfs(edges, 3)
