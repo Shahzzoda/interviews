4f2ef311ea5fc95dc5d0bff38b27bfe3025ad0fa
Shahzoda Davlatova <shahzoda01@gmail.com>
2020-01-22 03:52:49 -0500

    added the elementary sorting algos.

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -1,8 +1,49 @@
-# bubble sort
+# Bubble sort - compares adjacent pairs and swaps every time until sorted
+# Worst case is O(n^2) this happens when its reverse sorted
+# Best case is O(n) this happens when its sorted. I didn't implemented in 
+# my code but you can go through it once to see if it needs swapping, that's O(n).
+def bubblesort(arr):
+    for i in range(len(arr)): # go through whole array
+        for j in range(len(arr)-1-i): #go through the array up to n-1-i 
+        # (bc we access arr[j+1] and we already know the last i elements will 
+        # be sorted after our i'th passing.) 
+            print("comparing", arr[j], "and", arr[j+1])
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+        print("after one passing", arr)
 
-# selection sort
+# Selection sort - bubble sort brings the largest elements to the end slowly, 
+# in that sense, selection sort is the inverse of that (not exactly since we 
+# conpare pairs) in the sens that it always brings the smallest down to the front. 
+# Selection sort basically finds the smallest in the subarray and start to sort 
+# the first half of the array by swapping it to its right location.  
+# Runtime: O(n^2) 
+def selectionsort(arr):
+    for i in range(len(arr)): # the whole array O(n)
+        min = i
+        curr = i + 1
+        while curr < len(arr): # starting from i+1 to the end of the array 
+            # this is still considered O(n) 
+            if arr[curr] < arr[min]:
+                min = curr
+            curr += 1
+        arr[i], arr[min] = arr[min], arr[i]
+        print(i+1,":",arr)
 
-# insertion sort
+# insertion sort - keeps the left partition of the array sorted and looks
+# directly right of the partition to see if its larger the last elemented 
+# in the sorted partition. If it is, it keeps swapping to the left until it
+# determines the right place to insert the new element.
+# Worst case you shift every thing over to insert for every element. therefore
+# O(n^2)
+def insertionsort(arr):
+    for i in range(len(arr)): # O(n)
+        j = i + 1  # adj to the partion and the elem we will be inserting
+        while j - 1 >= 0 and j < len(arr) and arr[j] < arr[j-1]: # while index 
+            # is valid and j needs to have things shifted over to insert.
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+            j -= 1 # keep going further left to check if the item is at the right place
+        print(arr)
 
 # merge sort
 
